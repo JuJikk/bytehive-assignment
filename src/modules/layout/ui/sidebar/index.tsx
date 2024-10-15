7e2fc6bcd78ca9b7f549384c9a014b4482fc2905
marko <markomarynovych@gmail.com>
2024-10-15 17:37:39 +0300

    fix: fix sidebar links background

--- src/modules/layout/ui/sidebar/index.tsx
+++ src/modules/layout/ui/sidebar/index.tsx
@@ -24,17 +24,17 @@ import { NavItem } from "../../types";
 import {useLocation} from "react-router-dom";
 
 const mainNavItems: NavItem[] = [
-  { id: 0, title: "Overview", link: "", icon: <Home /> },
-  { id: 1, title: "Analytics", link: "analytics", icon: <BarChart /> },
-  { id: 2, title: "Ecommerce", link: "ecommerce", icon: <ShoppingCart /> },
-  { id: 3, title: "Crypto", link: "crypto", icon: <CurrencyBitcoin /> },
+  { id: 0, title: "Overview", link: "/", icon: <Home /> },
+  { id: 1, title: "Analytics", link: "/analytics", icon: <BarChart /> },
+  { id: 2, title: "Ecommerce", link: "/ecommerce", icon: <ShoppingCart /> },
+  { id: 3, title: "Crypto", link: "/crypto", icon: <CurrencyBitcoin /> },
 ];
 
 const analyticsNavItems: NavItem[] = [
-  { id: 4, title: "Products", link: "products", icon: <Inventory /> },
-  { id: 5, title: "Orders", link: "orders", icon: <ShoppingBasket /> },
-  { id: 6, title: "Invoices", link: "invoices", icon: <Receipt /> },
-  { id: 7, title: "Logistics", link: "logistics", icon: <LocalShipping /> },
+  { id: 4, title: "Products", link: "/products", icon: <Inventory /> },
+  { id: 5, title: "Orders", link: "/orders", icon: <ShoppingBasket /> },
+  { id: 6, title: "Invoices", link: "/invoices", icon: <Receipt /> },
+  { id: 7, title: "Logistics", link: "/logistics", icon: <LocalShipping /> },
 ];
 
 const Sidebar = () => {
